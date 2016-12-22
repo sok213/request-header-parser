6a927632e7ed551c941d1bf05b7406245e9f3444
sok213 <sophanarithsok@gmail.com>
2016-12-21 16:32:38 -0800

    implement user agent

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express'),
 	router = express.Router(),
 	ip = require('ip'),
-	os = require('os');
+	os = require('os'),
+	useragent = require('useragent');
 
 router.get('/', function(req, res, next) {
+	
+	var agent = useragent.parse(req.headers['user-agent']);
+	console.log(agent.toString());
+
 	res.render('index', {
 		title: 'Request Header Parser Microservice',
 		lang: req.headers['accept-language'].split(',')[0],
-		ip: ip.address(),
-		os: os.platform() + ' ' + os.arch()
+		ip: req.ip,
+		os: agent.toString()
 	});
 });
 
