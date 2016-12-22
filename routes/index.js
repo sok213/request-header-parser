3f19805c220e4843a295d2a9d608b62ea587a52c
sok213 <sophanarithsok@gmail.com>
2016-12-21 17:10:59 -0800

    remove os version

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,13 @@ var express = require('express'),
 	useragent = require('useragent');
 
 router.get('/', function(req, res, next) {
-	
 	var agent = useragent.parse(req.headers['user-agent']);
-	console.log(agent);
-
 	res.render('index', {
 		title: 'Request Header Parser Microservice',
 		lang: req.headers['accept-language'].split(',')[0],
 		ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
-		os: agent.toString()
+		os: agent.toString().split('/')[0] + '/' + 
+			agent.toString().split('/')[1].replace(/[0.]/g, '' )
 	});
 });
 
